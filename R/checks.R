f5654be5716992deb59cae74a6e05f0b6605963e
hanzholahs <hanzholahs@gmail.com>
2023-02-12 17:59:45 +0700

    Add function to check if an algorithm exists

--- R/checks.R
+++ R/checks.R
@@ -19,3 +19,21 @@ stop_if_no_system_dataset <- function(proj_name, system_dataset_name) {
          call. = FALSE)
   }
 }
+
+stop_if_no_algorithm <- function(algorithm_name, algorithm_type) {
+  algorithm_list <- pss_get("algorithm", algorithm_type) |>
+    parse_response("json") |>
+    purrr::map_chr("algoId")
+
+  if (!algorithm_name %in% algorithm_list) {
+    stop(paste0("The algorithm `", algorithm_name, "` with type of `",
+                algorithm_type, "` does not exist."),
+         call. = FALSE)
+  }
+}
+
+stop_if_no_any_algorithms <- function(algorithm_list) {
+  for (algorithm in algorithm_list) {
+    stop_if_no_algorithm(algorithm$algorithmName, algorithm$type)
+  }
+}
